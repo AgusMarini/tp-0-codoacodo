006fbfb435a989cd2dc8c7d539c900bcace76942
AgusMarini <Agustina_1312@hotmail.com>
2022-10-17 12:02:30 -0300

    arreglos

--- js/formulario.js
+++ js/formulario.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
@@ -15,8 +14,8 @@ const validarFormulario = (e) => {
         if(expresiones.usuario.test(e.target.value)){
             document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto');  
             document.getElementById('grupo__usuario').classList.add('formulario__grupo-correcto');  
-            document.querySelector('#grupo__usuario i').classList.add('fa-check-circle');
-            document.querySelector('#grupo__usuario i').classList.remove('fa-times-circle');
+            document.querySelector('#grupo__usuario i').classList.add('fa-solid fa-circle-xmark');
+            document.querySelector('#grupo__usuario i').classList.remove('fa-solid fa-circle-xmark');
             document.querySelector('#grupo_usuario .formulario__input-error').classList.remove('formulario__input-error-activo');
         } else {
             document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
@@ -59,57 +58,4 @@ inputs.forEach((input) => {
   formulario.addEventListener('submit', (e) => {
      e.preventDefault();
     });
-
-=======
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
-
-const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-}
-const validarFormulario = () => {
-   switch (e.target.name) {
-    case "usuario":
-        if(expresiones.usuario.test(e.target.value)){
-            document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto');  
-            document.getElementById('grupo__usuario').classList.add('formulario__grupo-correcto');  
-        } else {
-            document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
-        }
-        
-        break;
-    case "nombre":
-        
-        break;
-    case "password":
-        
-        break;
-    case "password2":
-        
-        break;
-    case "correo":
-        
-        break;
-    case "telefono":
-        
-        break;
-
-   }
- 
-}
-
-inputs.forEach((input) => {
-
- input.addEventListener('keyup', validarFormulario);
- input.addEventListener('blur', validarFormulario);
- })
-
-
-formulario:addEventListener('submit', (e) => {
- e.preventDefault();
-});
 
